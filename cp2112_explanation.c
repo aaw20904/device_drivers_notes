4c7232ec59f6ce6ef4cecd75b39619298f982028
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2025-07-28 19:29:39 +0300

    Update cp2112_explanation.c

--- cp2112_explanation.c
+++ cp2112_explanation.c
@@ -1,1 +1,13 @@
+/**
+DRIVER AND SDK installation
 
+Load page for drivers (MacOS, Linux, Windows)
+https://www.silabs.com/development-tools/interface/cp2112ek-development-kit?tab=software-tools
+for the Windows is here:
+https://www.silabs.com/documents/public/software/USBXpressHostSDK-Win.zip
+Run the installer USBXpressHostSDK-6.7.7-Win.msi
+After installation there will be main folder "SiliconLabs" on the computer.
+1)The dinamic link library  is here :   D:\SiliconLabs\USBXpressHostSDK\CP2112\Release\x64\SLABHIDtoSMBus.dll,
+  moreover there are also statical and Python libraries.
+2)The header  file is here:  D:\SiliconLabs\USBXpressHostSDK\CP2112\include\SLABHIDDevice.h
+*/
