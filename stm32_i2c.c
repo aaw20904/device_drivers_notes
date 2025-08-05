4edd338284b7f8cf7f18eb13cbed4e4aa183c46a
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2025-08-05 19:55:00 +0300

    Update stm32_i2c.c

--- stm32_i2c.c
+++ stm32_i2c.c
@@ -229,6 +229,169 @@ void I2C2_IRQHandler(void)
 }
 
 
+/*****99999999999999999999999999999999***/
+int MasterReadOne(uint8_t addr, uint8_t *dataPtr)
+{
+	/*
+	there should be set -
+	(1)interrupts STOP (set STOPIE) ,
+	     clear the semaphore 'states_I2C' in this interrupt handler
+	(2)Interrupts RXNE. In the interrupts a new data stores into buffer
+	 ******
+	 INTERRUPT NOTE:
+	 S T O P:  clear index of the array to 0, clear the semaphore
+	 R X N E:  read new data into array by index
+	*/
+	   //1) Enable hardware slave control
+	    I2C2->CR1 |= I2C_CR1_SBC;
+	   //1) Add the delays SDADEL, SCLDEL in TIMINGR (optionally)
+	   I2C2->TIMINGR |=  (0x0F << 16) |  //SDA delay
+			             (0x0F << 20); //SCL delay
+	    //2) Initialize global buffer address
+	     i2c2RxDataPtr = dataPtr;
+		//3) set auto-end, amount of data, hardware control (number of transmitted data)
+		I2C2->CR2 |= (I2C_CR2_AUTOEND |  //auto end
+				  (addr << 1) |  //address
+				  (1 << 10) |   //  R E A D   operation
+				  (1 << 16) );  //one byte
+		//NOTE: In RXNE interrupt data stores in array, index++;
+		//      In STOP index = 0
+		//5) Start transmission, reset state semaphore firstly:
+		states_I2C |= I2C2_PACKET;
+		//6)Start transaction
+		I2C2->CR2 |= I2C_CR2_START;
+		//7)Waiting: the semaphore will has been cleared in the ISR (STOP flag)
+		while (states_I2C &  I2C2_PACKET );
+
+		if (I2C2->ISR &  I2C_ISR_NACKF) {
+			//when NACK = return  error
+			return -1;
+		} else {
+			return 0;
+		}
+
+}
+
+void slaveTxOneInit(uint8_t address, uint8_t* buffer) {
+ /**NOTE: there should be done before the following:
+	     (1)Load own address into  OAR1 and set here OA!EN
+		 (2)STOP (set STOPIE ). The interrupt procedure clears the semaphore 'states_I2C'
+	       and terminate while() loop.
+	     (3) TXIS (set TXIE).This interrupt appears when numBytes > 1.
+	       So the last transmitted byte NOT call the interrupt.
+	     (4) ADDR (set (ADDRIE).Load the first byte here. Clear this bit in the interrupt by the ADDRCF flag
+	     ****************
+	     INTERRUPTS:
+	     S T O P: clears the semaphore 'states_I2C'.Clears index to 0
+	     T X I S: write new data from the buffer-array, by the index, after this index++;
+		 **/
+	//1) Init RAM buffer for slave
+  i2c1TxDataPtr = buffer;
+    //2)assign address
+  I2C1->OAR1 |= (address << 1) | I2C_OAR1_OA1EN;
+  ///all the work to receive data be done in the ISR
+
+}
+
+/*NOTE!! The first byte must be written before transmission manually! Code has a bug - because in ADDR 
+  Interrupt of slave transmitter data doesn`t used (slave got data after this interrupt ***/
+int MasterReadPacket(uint8_t addr, uint8_t size, uint8_t* dataPtr)
+{
+	/*
+	there should be set -
+	(1)interrupts STOP (set STOPIE) ,
+	     clear the semaphore 'states_I2C' in this interrupt handler
+	(2)Interrupts RXNE. In the interrupts a new data stores into buffer
+	 ******
+	 INTERRUPT NOTE:
+	 S T O P:  clear index of the array to 0, clear the semaphore
+	 R X N E:  read new data into array by index
+	*/
+	//clear error flags
+	I2C2->ICR = I2C_ICR_NACKCF |  I2C_ICR_BERRCF |  I2C_ICR_ARLOCF;
+
+	   //1) Enable hardware slave control
+	    I2C2->CR1 |= I2C_CR1_SBC;
+	   //1) Add the delays SDADEL, SCLDEL in TIMINGR (optionally)
+	   I2C2->TIMINGR |=  (0x0F << 16) |  //SDA delay
+			             (0x0F << 20); //SCL delay
+	    //2) Initialize global buffer address
+	     i2c2RxDataPtr = dataPtr;
+		//3) set auto-end, amount of data, hardware control (number of transmitted data)
+		I2C2->CR2 |= (I2C_CR2_AUTOEND |  //auto end
+				  (addr << 1) |  //address
+				  (1 << 10) |   //  R E A D   operation
+				  (size << 16) );  //amount of data
+		//NOTE: In RXNE interrupt data stores in array, index++;
+		//      In STOP index = 0
+		//5) Start transmission, reset state semaphore firstly:
+		states_I2C |= I2C2_PACKET;
+		//6)Start transaction
+		I2C2->CR2 |= I2C_CR2_START;
+		//7)Waiting: the semaphore will has been cleared in the ISR (STOP flag)
+		while (states_I2C &  I2C2_PACKET );
+
+
+    if (I2C2->ISR & (I2C_ISR_NACKF |  I2C_ISR_BERR| I2C_ISR_ARLO ))
+        return -1;
+    else
+        return 0;
+}
+
+//--------------interrupts-------------------------
+void I2C1_IRQHandler(void)
+{
+  /* USER CODE BEGIN I2C1_IRQn 0 */
+   ///------T X   m u s t  b e   w r i t t e n-----
+	if (I2C1->ISR & I2C_ISR_TXIS) {
+		//read data from a buffer into the transmitter
+		I2C1->TXDR = i2c1TxDataPtr[txCounterI2C1++];
+	}
+     //----a d d r e s s---m a t c h---
+	if (I2C1->ISR & I2C_ISR_ADDR) {
+		//load the first byte
+		I2C1->TXDR = i2c1TxDataPtr[txCounterI2C1++];
+		//clear address match
+		I2C1->ICR = I2C_ICR_ADDRCF;
+	}
+   ///---------s t o p-------
+	if (I2C1->ISR & I2C_ISR_STOPF) {
+	        // Clear STOPF
+	        I2C1->ICR |= I2C_ICR_STOPCF;
+            //clear index of the pointer
+	        txCounterI2C1 = 0;
+	        // Set your flag/semaphore
+	        states_I2C &= ~I2C1_PACKET;
+	 }
+
+
+  /* USER CODE END I2C1_IRQn 1 */
+}
+
+/**
+  * @brief This function handles I2C2 global interrupt.
+  */
+void I2C2_IRQHandler(void)
+{
+  /* USER CODE BEGIN I2C2_IRQn 0 */
+///means-------- M A S T E R --------
+
+    //---------RX not empty
+	if (I2C2->ISR & I2C_ISR_RXNE) {
+	      i2c2RxDataPtr[rxCounterI2C2++] = I2C2->RXDR;
+	}
+
+	if (I2C2->ISR & I2C_ISR_STOPF) {
+	        // Clear STOPF
+	        I2C2->ICR |= I2C_ICR_STOPCF;
+            //clear index of the pointer
+	        rxCounterI2C2 = 0;
+	        // Set your flag/semaphore
+	        states_I2C &= ~I2C2_PACKET;
+	 }
+
+  /* USER CODE END I2C2_IRQn 1 */
+}
 
 
 
